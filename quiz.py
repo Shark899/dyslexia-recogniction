e946e1612ca6f8468b991bd44193fb75286fad79
Federico Franciosi <gigante.89@hotmail.it>
2023-05-20 22:54:20 +0200

    First Update
    Updated quiz questions(Console )

---
+++ quiz.py
@@ -0,0 +1,16 @@
+quiz = ["Hai difficoltà a distinguere la destra dalla sinistra?",
+        "Ti confonde la lettura di una mappa o l'orientarti in un posto nuovo?", "Detesti leggere a voce alta?",
+        "Ci metti più tempo del dovuto a leggere una pagina di un libro?",
+        "Trovi difficile ricordare il significato di ciò che hai letto?", "Detesti leggere libri lunghi?",
+        "La tua ortografia è scadente?", "La tua calligrafia è difficile da leggere?",
+        "Ti confondi se devi parlare in pubblico?",
+        "Hai difficoltà a prendere un messaggio telefonico e trasmetterlo correttamente?",
+        "Quando pronunci una parola lunga, ti capita di avere difficoltà a mettere i vari suoni nell'ordine corretto ?",
+        "Hai difficoltà a sommare mentalmente,senza usare le dita o un pezzo di carta?",
+        "Quando usi il telefono,ti capita di confondere le diverse cifre mentre marchi il numero?",
+        "Hai difficoltà a ripetere i mesi dell'anno uno dopo l'altro in modo scorrevole?",
+        "Hai difficoltà a ripetere i mesi dell'anno a ritroso?", "Confondi date e orari e dimentichi appuntamenti?",
+        "Quando scrivi degli assegni ti capita spesso di fare errori?"
+    , "Trovi che i furmolari siano difficili e confondano?", "Confondi numeri dell'autobus come il 59 e il 95?",
+        "Hai avuto difficoltà ad imparare le tabelline a scuola?"
+        ]
