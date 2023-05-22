67e371eb618c9e7958241f837438872c1c7f9f08
Federico Franciosi <gigante.89@hotmail.it>
2023-05-22 16:41:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import simpleaudio as sa
-from quiz import quiz
+from quiz import quiz,other_quiz
+from PIL import Image
+
+image1 = Image.open(r"./beer.png")
+image1.show()
 
 choice = 0
 score = 0
 words = ["tavolo", "lavandino", "torta"]
 description = "Ti presenterò 3 parole avrai un tempo limite di 10 secondi a parola per riscriverla e premere Enter"
+
+print("Ciao! Mi chiamo " + "Età" + ".")
+print("Sono stato creato nel " + "2023" + ".")
+print("Per favore, ricordami il tuo nome.")
 print("Welcome to dyslexia recogniction")
 print("1) Text Test ")
 print("2) Speech Test ")
-choice = int(input("Select a test:(1,2 or 3)"))
+print("3) Dyslexia Quiz ")
+print("4) Assete Presente")
+
+choice = int(input("Select a test:(1,2,3 o 4)"))
 if choice == 1:
     for i in range(0, len(words)):
         print(words[i])
@@ -49,4 +60,21 @@ if choice == 3:
         print("Sei sicuramente dislessico")
     else:
         print(f"il tuo punteggio è:{score}/{len(quiz)}")
+        print("Non sei dislessico")
+if choice == 4:
+    answer = ""
+    for i in other_quiz:
+        print(f"{i}")
+        answer = input("Assente  Presente  Molto Presente")
+        answer = answer.lower()
+        if answer == "assente":
+            score += 1
+    if score > 5 and score < 9:
+        print(f"il tuo punteggio è:{score}/{len(other_quiz)}")
+        print("Probabilmente sei dislessico")
+    elif score > 9:
+        print(f"il tuo punteggio è:{score}/{len(other_quiz)}")
+        print("Sei sicuramente dislessico")
+    else:
+        print(f"il tuo punteggio è:{score}/{len(other_quiz)}")
         print("Non sei dislessico")
